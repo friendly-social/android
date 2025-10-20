d73809b6c5a19b79369ed5bb6070f765f1ad8b22
demndevel <demndevel@gmail.com>
2025-10-20 21:31:41 +0300

    reformat all of this stuff

--- app/src/main/java/friendly/android/ProfileScreen.kt
+++ app/src/main/java/friendly/android/ProfileScreen.kt
@@ -4,19 +4,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import app.cash.molecule.RecompositionMode.ContextClock
-import app.cash.molecule.launchMolecule
+import androidx.compose.ui.tooling.preview.Preview
 import friendly.android.ProfileScreenModel.Data.UserDetails
 import friendly.android.ProfileScreenModel.Loading
 import friendly.sdk.Interest
@@ -24,12 +22,9 @@ import friendly.sdk.Nickname
 import friendly.sdk.UserDescription
 import friendly.sdk.UserId
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.StateFlow
 
 sealed interface ProfileScreenModel {
-    data class Data(
-        val userDetails: UserDetails,
-    ) : ProfileScreenModel {
+    data class Data(val userDetails: UserDetails) : ProfileScreenModel {
         data class UserDetails(
             val nickname: Nickname,
             val description: UserDescription,
@@ -41,20 +36,19 @@ sealed interface ProfileScreenModel {
 }
 
 @Composable
-fun ProfileScreenModel(
-    userId: UserId,
-): ProfileScreenModel {
+fun ProfileScreenModel(userId: UserId): ProfileScreenModel {
     var data: ProfileScreenModel by remember { mutableStateOf(Loading) }
 
     LaunchedEffect(Unit) {
         println("user id: ${userId.long}")
-
         delay(1500L)
 
         data = ProfileScreenModel.Data(
             UserDetails(
                 nickname = Nickname.orThrow("y9kap"),
-                description = UserDescription.orThrow("mikael fridman big friend"),
+                description = UserDescription.orThrow(
+                    "mikael fridman big friend",
+                ),
                 interests = buildList {
                     add(Interest.orThrow("cooking"))
                     add(Interest.orThrow("singing"))
@@ -70,10 +64,7 @@ fun ProfileScreenModel(
 }
 
 @Composable
-fun ProfileScreen(
-    userId: UserId,
-    modifier: Modifier = Modifier,
-) {
+fun ProfileScreen(userId: UserId, modifier: Modifier = Modifier) {
     val state = ProfileScreenModel(userId)
 
     Column(
@@ -95,5 +86,20 @@ fun ProfileScreen(
                 CircularProgressIndicator()
             }
         }
+    }
+}
+
+@Preview
+@Composable
+private fun ProfileScreenPreview() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize(),
+    ) {
+        ProfileScreen(
+            userId = UserId(10_000L),
+            modifier = Modifier
+                .fillMaxSize(),
+        )
     }
 }
--- app/src/main/java/friendly/android/RegisterScreen.kt
+++ app/src/main/java/friendly/android/RegisterScreen.kt
@@ -13,13 +13,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
 
 @Composable
-fun RegisterScreen(
-    onHome: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
+fun RegisterScreen(onHome: () -> Unit, modifier: Modifier = Modifier) {
     Column(modifier) {
         Row(
             modifier = Modifier
