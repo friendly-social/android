18e65dc47d10c6cecda5198b2105dbb77849d50e
demndevel <demndevel@gmail.com>
2026-07-12 00:44:38 +0300

    fix(sign-in): add missing ime padding

--- app/src/main/java/friendly/android/SignInScreen.kt
+++ app/src/main/java/friendly/android/SignInScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
@@ -133,6 +134,7 @@ fun SignInScreen(
                     state = state,
                     modifier = Modifier
                         .padding(innerPadding)
+                        .imePadding()
                         .fillMaxSize(),
                 )
             }
